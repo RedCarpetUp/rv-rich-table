d3cf3e7c2437ba47ad12d3d2ccbbd3a446219b40
iamrajhans <rajhans.jadhao@gmail.com>
2017-11-29 20:26:21 +0530

    passing all props to component

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,18 @@
-import React, { Component } from "react";
-import { Route, Link, Switch } from "react-router-dom";
-import { inject, observer } from "mobx-react";
-import LazyRoute from "lazy-route";
-import DevTools from "mobx-react-devtools";
+import React, {Component} from "react";
+import {Route, Switch} from "react-router-dom";
 import Profile from './Profile';
 
 export default class Routes extends Component {
 
-	render() {
-		const { appState } = this.props;
-		return (
-			<div className="wrapper">
-			<Switch>
-				<Route exact path={"/"} component={({match}) => <Profile match={match} appState={appState}/>}/>
-				<Route exact path={"/profiles/:page?/:sort?/:order?"} component={({match}) => <Profile match={match} appState={appState}/>}/>
-			</Switch>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="wrapper">
+                <Switch>
+                    <Route exact path={"/"} component={(props) => <Profile {...props} />}/>
+                    <Route exact path={"/profiles/:page?/:sort?/:order?"}
+                           component={(props) => <Profile {...props} />}/>
+                </Switch>
+            </div>
+        );
+    }
 }
