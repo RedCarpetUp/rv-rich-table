a1a6b441b27f420bece3dbacfd102a0fdb3088c0
iamrajhans <rajhans.jadhao@gmail.com>
2017-11-30 18:12:14 +0530

    added constant values for Grid data - columns,columnName

---
+++ src/components/utils/constants.js
@@ -0,0 +1,5 @@
+export const PROFILEDATA = {
+    columns: ["select", "id", "avatar_url", "login", "followers_url", "task", "action"],
+    columnName: [" ", "Id", "Avatar", "Name", "Followers", "Task", "Action"],
+    columnWidths: [50, 140, 100, 150, 320, 170, 230],
+}
