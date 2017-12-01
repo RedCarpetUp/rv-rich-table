d8409417574a270cfbd59a3fe97f681372f0cbbb
iamrajhans <rajhans.jadhao@gmail.com>
2017-12-01 16:23:12 +0530

    added history in browser component and set default route to /profile

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,21 @@
 import React, {Component} from "react";
-import {Route, Switch} from "react-router-dom";
+import {Redirect, Route, Router, Switch} from "react-router-dom";
 import Profile from './Profile';
+import history from './history';
 
 export default class Routes extends Component {
 
     render() {
         return (
             <div className="wrapper">
-                <Switch>
-                    <Route exact path={"/"} component={(props) => <Profile {...props} />}/>
-                    <Route exact path={"/profiles/:page?/:sort?/:order?"}
-                           component={(props) => <Profile {...props} />}/>
-                </Switch>
+                <Router history={history}>
+                    <Switch>
+                         <Redirect exact path={"/"} to="/profiles"/>
+                        <Route path={"/profiles"} component={(props) => <Profile {...props} />}/>
+                        <Route exact path={"/profiles/:page?/:sort?/:order?"}
+                               component={(props) => <Profile {...props} />}/>
+                    </Switch>
+                </Router>
             </div>
         );
     }
